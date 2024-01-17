4413f4853394bc9262da8105cb9cd04ffaa8ebb7
Anant Prakash Singh <95313442+sinxhanant@users.noreply.github.com>
2024-01-17 08:59:14 +0530

    setup gh-pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import Home from './Home'
 import {Route, Switch} from 'react-router-dom'
@@ -9,13 +9,13 @@ import Contact from './Contact'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <HashRouter>
    <Route exact path ="/" component={Home}/>
    <Route exact path ="/userreg" component={App}/>
    <Route exact path ="/contact" component={Contact}/>
    <Route exact path ="/about" component={About}/>
 
-    </BrowserRouter>,
+    </HashRouter>,
 
 );
 
